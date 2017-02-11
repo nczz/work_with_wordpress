51ac3261ff9b945b5f8104f5c648ec508f284b4f
Chiang Hung Chun <im@mxp.tw>
2017-02-11 23:36:07 +0800

    move mxp-dev-tools to WordPress official svn repository

--- mxp_dev_tools/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * plugin should create a file named ‘uninstall.php’ in the base plugin folder. This file will be called, if it exists,
- * during the uninstall process bypassing the uninstall hook.
- * ref: https://developer.wordpress.org/reference/functions/register_uninstall_hook/
- */
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-	die;
-}
